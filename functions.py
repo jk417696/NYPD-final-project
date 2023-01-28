7a3b7f68d135ce9ce7516938ffee3520d5a4db4a
Justyna Król <jk417696@khaki08.mimuw.edu.pl>
2023-01-28 13:49:33 +0100

    added functions for data analysis and possibilty to specify years for which the analysis should be conducted, modified main

--- functions.py
+++ functions.py
@@ -2,50 +2,77 @@ import argparse
 import pandas as pd
 import numpy as np
 
-#parsing arguments (paths to csv files) from command line
+
+# parse arguments (paths to csv files) from command line
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("gdp", help="path to csv file with gdp data")
-    parser.add_argument("population", help="path to csv file with population data")
-    parser.add_argument("co2", help="path to file with co2 emission data")
+    parser.add_argument('gdp', help='path to csv file with gdp data')
+    parser.add_argument('population', help='path to csv file with population data')
+    parser.add_argument('co2', help='path to file with co2 emission data')
+    parser.add_argument('-start', type=int, default=None, help='start year for data analysis')
+    parser.add_argument('-end', type=int, default=None, help='end year for data analysis')
     arguments = parser.parse_args()
     return arguments
 
+
 def clean(data):
+    # change the format of country names so it is the same in all csv files
     if 'Country Name' in data.columns:
-        data = data.rename(columns = {'Country Name':'Country'})
+        data = data.rename(columns={'Country Name': 'Country'})
         data['Country'] = data['Country'].str.upper()
-        data = data.iloc[:,:-1]
+    # remove columns with only NaN values
+    data = data.dropna(axis=1, how='all')
     return data
 
 
-#returning years that are included in all three files
+# returns a list of years that are included in all three files
 def years(gdp, population, co2):
     years_co2 = co2['Year'].drop_duplicates()
-    years_gdp = pd.DataFrame({'Year':list(gdp.columns[4:-1])})
-    years_gdp = years_gdp['Year'].astype('int')
-    years_population = pd.DataFrame({'Year':list(population.columns[4:-1])})
-    years_population = years_population['Year'].astype('int')
-    years = pd.merge(years_co2, years_gdp, how = 'inner')
-    years = pd.merge(years, years_population, how = 'inner')
+    years_gdp = pd.DataFrame({'Year': list(gdp.columns[4:])})
+    years_gdp = years_gdp['Year'].astype('int64')
+    years_population = pd.DataFrame({'Year': list(population.columns[4:])})
+    years_population = years_population['Year'].astype('int64')
+    years = pd.merge(years_co2, years_gdp, how='inner')
+    years = pd.merge(years, years_population, how='inner')
     years = (years['Year'])
     return years
 
-#merging data on years and countries
+# cut years to those specified in the command line (if possible)
+def cut_years(years, start, end):
+    if start is None:
+        start = min(years)
+    if end is None:
+        end = max(years)
+    if start <= end:
+        years = years[(years >= start) & (years <= end)]
+        if len(years):
+            return years
+        else:
+            print('Error: no data available for years ', start, ' to ', end)
+            return []
+    else:
+        print('Error: start year higher than end year')
+        return []
+
+
+
+# merge data on years and countries
 def merge_data(gdp, population, co2, years):
-    #only look at co2 data for years that show up in all csv files
+    # only look at co2 data for years that show up in all csv files
     data = co2[co2['Year'].isin(years)]
-    #create data frame with gdp data such that one row contains data about gdp in one country for a specific year (format closer to co2 data)
-    gdp_split = pd.DataFrame(columns = gdp.columns[0:4])
-    gdp_one_year = gdp.iloc[:,0:4]
+    # create data frame with gdp data such that
+    # one row contains data about gdp in one country for a specific year
+    # (format closer to co2 data)
+    gdp_split = pd.DataFrame(columns=gdp.columns[0:4])
+    gdp_one_year = gdp.iloc[:, 0:4]
     gdp_one_year['GDP'] = np.nan
     for year in years:
         gdp_one_year['Year'] = [year] * len(gdp)
         gdp_one_year['GDP'] = gdp[str(year)]
         gdp_split = gdp_split.append(gdp_one_year)
-    #merge co2 data with gdp data in a new format
-    data = data.merge(gdp_split, on = ['Country', 'Year'], how = 'outer')
-    #create data frame with population data such that one row contains data about population in one country for a specific year (format closer to co2 data)
+    # merge co2 data with gdp data in a new format
+    data = data.merge(gdp_split, on=['Country', 'Year'], how='outer')
+    # create data frame with population in the same format as above
     pop_split = pd.DataFrame(columns=population.columns[0:4])
     pop_one_year = population.iloc[:, 0:4]
     pop_one_year['Population'] = np.nan
@@ -53,7 +80,56 @@ def merge_data(gdp, population, co2, years):
         pop_one_year['Year'] = [year] * len(population)
         pop_one_year['Population'] = population[str(year)]
         pop_split = pop_split.append(pop_one_year)
-    #merge data about co2, gdp and population in new format
+    # merge data about co2, gdp and population in new format
     data = data.merge(pop_split, on=['Country', 'Year', 'Country Code'], how='outer')
+    data['Year'] = data['Year'].astype('int64')
     return data
 
+
+# returns a table with 5 most emitting countries (per capita) for all years
+# all the information needed is in co2 data therefore we only put this file as an argument
+def worst_emitters(co2, years):
+    # include only columns of interest
+    df = co2[['Year', 'Country', 'Total', 'Per Capita']]
+    # create empty data frame for the results
+    emitters = pd.DataFrame(columns=['Year', 'Country', 'Total', 'Per Capita'])
+    # for each year find top 5 emitting countries (per capita) and add them to emitters data frame
+    for year in years:
+        df_temp = df[df['Year'] == year]
+        df_temp = df_temp.sort_values(by=['Per Capita'], ascending=False)
+        emitters = emitters.append(df_temp.head(5))
+    return emitters
+
+
+# returns a table with 5 countries with the highest gdp per capita in each year
+# as an argument we put the merged data
+def highest_gdp(data, years):
+    # include only columns of interest
+    df = data[['Year', 'Country', 'GDP', 'Population']]
+    # create empty data frame for the results
+    richest = pd.DataFrame(columns=['Year', 'Country', 'GDP', 'Population', 'Per Capita'])
+    # for each year find top 5 countries with the highest gdp per capita and add them to the richest data frame
+    for year in years:
+        df_temp = df[df['Year'] == year]
+        df_temp['Per Capita'] = df_temp['GDP'] / df_temp['Population']
+        # sort the values and find top 5
+        df_temp = df_temp.sort_values(by=['Per Capita'], ascending=False)
+        richest = richest.append(df_temp.head(5))
+    # drop  column population since we dan't need it in our results
+    richest = richest.drop(columns=['Population'])
+    return richest
+
+
+# returns a change in co2 emission in the last 10 years for each country
+def emission_change(co2, year):
+    # include only columns of interest
+    df = co2[['Year', 'Country', 'Per Capita']]
+    # find emission in the last years and 10 years prior
+    emission_now = df[df['Year'].isin([year])]
+    emission_then = df[df['Year'].isin([year - 10])]
+    change = emission_now.merge(emission_then, on=['Country'])
+    # calculate the change in emission per capita
+    change['Change'] = change['Per Capita_x'] - change['Per Capita_y']
+    # sort the values
+    change = change[['Country', 'Change']].sort_values(by=['Change'])
+    return change
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
+import functions as f
 import pandas as pd
-import numpy as np
-import argparse
 
-def main(gdp_path, population_path, co2_path):
-    gdp = pd.read_csv(gdp_path)
-    population = pd.read_csv(population_path)
-    co2 = pd.read_csv(co2_path)
 
-#testowy komentarz
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("gdp", help="path to csv file with gdp data")
-    parser.add_argument("population", help="path to csv file with population data")
-    parser.add_argument("co2", help = "path to file with co2 emission data")
-    arguments = parser.parse_args()
-    return arguments
+def main(gdp_path, population_path, co2_path, start, end):
+    # reading csv files
+    # files gdp and population had comments in first 4 rows, so we need to skip these rows
+    gdp = f.clean(pd.read_csv(gdp_path, skiprows=4))
+    population = f.clean(pd.read_csv(population_path, skiprows=4))
+    co2 = f.clean(pd.read_csv(co2_path))
+    # return years for which the analysis will be conducted
+    years = f.years(gdp, population, co2)
+    # if some years were specified in the command line
+    if ((start is not None) | (end is not None)):
+        years = f.cut_years(years, start, end)
+        # if incorrect years were specified - show the warning and stop the program
+        if not len(years):
+            return 0
+    # analyse the data
+    data = f.merge_data(gdp, population, co2, years)
+    change = f.emission_change(co2, max(years))
+    worst_emitters = f.worst_emitters(co2, years)
+    highest_gdp = f.highest_gdp(data, years)
+    print('Top 5 co2 emitting countries for each year: \n',
+          worst_emitters)
+    print('Top 5 countries with the highest GDP per capita for each year: \n',
+          highest_gdp)
+    print('Country with the biggest increase in emission per capita from ', max(years)-10, ' to ', max(years),': \n',
+          change.iloc[-1])
+    print('Country with the biggest decrease in emission per capita from ', max(years)-10, ' to ', max(years),': \n',
+          change.iloc[0])
+    return 0
+
 
 if __name__ == '__main__':
-    arguments = parse_arguments()
-    main(arguments.gdp, arguments.population, arguments.co2)
+    # parsing arguments from command line
+    arguments = f.parse_arguments()
+    # main function
+    main(arguments.gdp, arguments.population, arguments.co2, arguments.start, arguments.end)
